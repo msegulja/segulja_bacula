672081c3d1d5b2a0002f089af9c2a4f1a0149d48
Michael Segulja <michael@segulja.com>
2018-03-10 12:34:11 -0600

    Initial Commit; Basic test kitchen config; Basic initial tests written; Version 0.1.0

---
+++ test/smoke/default/default_test.rb
@@ -0,0 +1,32 @@
+# # encoding: utf-8
+
+# Inspec test for recipe segulja_bacula::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+describe package('mt-st') do
+  it { should be_installed }
+end
+
+describe directory('/opt/bacula/') do
+  it { should exist }
+end
+
+describe file('opt/bacula/etc/bacula-dir.conf') do
+  it { should exist }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+end
+
+describe file('/opt/bacula/etc/bacula-sd.conf') do
+  it { should exist }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+end
+
+describe file('/opt/bacula/etc/bacula-fd.conf') do
+  it { should exist }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+end
