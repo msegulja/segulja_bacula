bbaf2748536fa0453868443ac7f22d748acb8549
Michael Segulja <michael@segulja.com>
2018-03-21 22:51:13 -0500

    Add director configure and compile back in

--- recipes/director.rb
+++ recipes/director.rb
@@ -68,37 +68,37 @@ bash 'Extract and configure the Bacula source code' do
   not_if { ::File.exist?('/tmp/bacula/bacula-9.0.6/README') }
 end
 
-# execute 'Configure Bacula Backup System from source code' do
-#   command 'cd /tmp/bacula/bacula-9.0.6 ; CFLAGS="-g -Wall" ./configure \
-#             --sbindir=/opt/bacula/bin \
-#             --sysconfdir=/opt/bacula/etc \
-#             --enable-smartalloc \
-#             --with-mysql \
-#             --with-working-dir=/opt/bacula/working \
-#             --with-pid-dir=/opt/bacula/working \
-#             --with-subsys-dir=/opt/bacula/working \
-#             --enable-conio \
-#             --with-archivedir=/opt/bacula/working/archive \
-#             --with-dump-email=michael@segulja.com'
-#   action :run
-#   live_stream true
-#   not_if { ::File.exist?('/tmp/bacula/bacula-9.0.6/config.out') }
-# end
+execute 'Configure Bacula Backup System from source code' do
+  command 'cd /tmp/bacula/bacula-9.0.6 ; CFLAGS="-g -Wall" ./configure \
+            --sbindir=/opt/bacula/bin \
+            --sysconfdir=/opt/bacula/etc \
+            --enable-smartalloc \
+            --with-mysql \
+            --with-working-dir=/opt/bacula/working \
+            --with-pid-dir=/opt/bacula/working \
+            --with-subsys-dir=/opt/bacula/working \
+            --enable-conio \
+            --with-archivedir=/opt/bacula/working/archive \
+            --with-dump-email=michael@segulja.com'
+  action :run
+  live_stream true
+  not_if { ::File.exist?('/tmp/bacula/bacula-9.0.6/config.out') }
+end
 
-# execute 'Compile and Install Bacula Backup System from source code' do
-#   command 'cd /tmp/bacula/bacula-9.0.6 ; make ; make install'
-#   action :run
-#   live_stream true
-#   not_if { :: File.exist?('/opt/bacula/bin/bacula') }
-# end
+execute 'Compile and Install Bacula Backup System from source code' do
+  command 'cd /tmp/bacula/bacula-9.0.6 ; make ; make install'
+  action :run
+  live_stream true
+  not_if { :: File.exist?('/opt/bacula/bin/bacula') }
+end
 
-# template '/opt/bacula/etc/bacula-dir.conf' do
-#   source 'bacula-dir.conf.erb'
-#   owner 'root'
-#   group 'root'
-#   mode '0755'
-#   action :create
-# end
+template '/opt/bacula/etc/bacula-dir.conf' do
+  source 'bacula-dir.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
 
 service 'mysqld' do
   action [:enable, :start]
