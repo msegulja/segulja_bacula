afb94eebcd517e06a1272e51c4ae9ac9f0349763
Michael Segulja <michael@segulja.com>
2018-03-21 22:04:02 -0500

    - Add test for the mysqld service   - Add test for the mt-st packag to be installed   - Remove test for mt-st package from the default test and add to the     director test   - Specify the version of the mysql package(s) to uninstall   - Specify the version of the mysql pacage(s) to install   - Comment out the configure and make execute resources temporarily to     test package uninstall and intall   - Version 0.1.0
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    On branch install-and-configure
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    modified:   metadata.rb
    modified:   recipes/director.rb
    modified:   test/smoke/default/default_test.rb
    modified:   test/smoke/default/director_test.rb

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,6 @@ long_description 'Installs/Configures segulja_bacula'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
--- recipes/director.rb
+++ recipes/director.rb
@@ -6,6 +6,7 @@
 
 package 'mysql-community-common' do
   action :remove
+  version ['5.7.16-1.el7']
 end
 
 %w(
@@ -13,16 +14,24 @@ end
   gcc
   gcc-c++
   make
-  postgresql
-  mariadb
-  mariadb-devel
-  mariadb-libs
   ncurses
   ncurses-devel
   ncurses-libs
 ).each do |pkg|
   package pkg do
     action :install
+  end
+end
+
+%w(
+  mysql-community-client
+  mysql-community-common
+  mysql-community-devel
+  mysql-community-libs
+  mysql-community-server
+).each do |pkg|
+  package pkg do
+    version ['5.6.39-2.el7']
   end
 end
 
@@ -59,34 +68,38 @@ bash 'Extract and configure the Bacula source code' do
   not_if { ::File.exist?('/tmp/bacula/bacula-9.0.6/README') }
 end
 
-execute 'Configure Bacula Backup System from source code' do
-  command 'cd /tmp/bacula/bacula-9.0.6 ; CFLAGS="-g -Wall" ./configure \
-                                          --sbindir=/opt/bacula/bin \
-                                          --sysconfdir=/opt/bacula/etc \
-                                          --enable-smartalloc \
-                                          --with-mysql \
-                                          --with-working-dir=/opt/bacula/working \
-                                          --with-pid-dir=/opt/bacula/working \
-                                          --with-subsys-dir=/opt/bacula/working \
-                                          --enable-conio \
-                                          --with-archivedir=/opt/bacula/working/archive \
-                                          --with-dump-email=michael@segulja.com'
-  action :run
-  live_stream true
-  not_if { ::File.exist?('/tmp/bacula/bacula-9.0.6/config.out') }
-end
+# execute 'Configure Bacula Backup System from source code' do
+#   command 'cd /tmp/bacula/bacula-9.0.6 ; CFLAGS="-g -Wall" ./configure \
+#             --sbindir=/opt/bacula/bin \
+#             --sysconfdir=/opt/bacula/etc \
+#             --enable-smartalloc \
+#             --with-mysql \
+#             --with-working-dir=/opt/bacula/working \
+#             --with-pid-dir=/opt/bacula/working \
+#             --with-subsys-dir=/opt/bacula/working \
+#             --enable-conio \
+#             --with-archivedir=/opt/bacula/working/archive \
+#             --with-dump-email=michael@segulja.com'
+#   action :run
+#   live_stream true
+#   not_if { ::File.exist?('/tmp/bacula/bacula-9.0.6/config.out') }
+# end
 
-execute 'Compile and Install Bacula Backup System from source code' do
-  command 'cd /tmp/bacula/bacula-9.0.6 ; make ; make install'
-  action :run
-  live_stream true
-  not_if { :: File.exist?('/opt/bacula/bin/bacula') }
-end
+# execute 'Compile and Install Bacula Backup System from source code' do
+#   command 'cd /tmp/bacula/bacula-9.0.6 ; make ; make install'
+#   action :run
+#   live_stream true
+#   not_if { :: File.exist?('/opt/bacula/bin/bacula') }
+# end
 
-template '/opt/bacula/etc/bacula-dir.conf' do
-  source 'bacula-dir.conf.erb'
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
+# template '/opt/bacula/etc/bacula-dir.conf' do
+#   source 'bacula-dir.conf.erb'
+#   owner 'root'
+#   group 'root'
+#   mode '0755'
+#   action :create
+# end
+
+service 'mysqld' do
+  action [:enable, :start]
 end
